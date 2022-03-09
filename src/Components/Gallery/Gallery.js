afef29d0e5ea310f278bf16bf95f791188f3721a
shermscissors <christian.gales09@gmail.com>
2022-03-09 13:17:52 -0500

    Fixing CSS

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,3 +1,6 @@
+//  CSS
+import './Gallery.css'
+
 // External Stuff
 import React from 'react';
 
@@ -5,6 +8,8 @@ import React from 'react';
 function About(props) {
 	return (
 		<div className='body-container'>
+			<h1>Gallery</h1>
+			<h2>Subheading Stuff</h2>
 			<span className='grid-template'>
 				
 				<img src="" alt="" srcset="" />
