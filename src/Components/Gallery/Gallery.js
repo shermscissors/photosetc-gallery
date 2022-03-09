9ab2663c19b140b3a7838ee1d77473a1333f9071
shermscissors <christian.gales09@gmail.com>
2022-03-09 14:09:00 -0500

    Lunch Break

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,20 +1,17 @@
 //  CSS
-import './Gallery.css'
+import './Gallery.css';
 
 // External Stuff
 import React from 'react';
 
-
 function About(props) {
 	return (
 		<div className='body-container'>
-			<h1>Gallery</h1>
-			<h2>Subheading Stuff</h2>
-			<span className='grid-template'>
+			<span className='gallery-container'>
 				<img src={'../../assets/456373006516-R1-035-16.jpg'} alt='' />
 				<img src={'../../assets/clouds.jpg'} alt='' />
 				<img src={'../../assets/DSCF0089.jpg'} alt='' />
-				<img src={'../../assets/DSCF0095.jpg'} alt='' />
+				<img src={'../../assets/DSCF0095.jpg'} alt='??' />
 			</span>
 		</div>
 	);
