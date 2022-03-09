b6ceb70d71f0013baf42585020c320c59db27eb3
shermscissors <christian.gales09@gmail.com>
2022-03-09 13:33:58 -0500

    Fixed Images?

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -11,8 +11,10 @@ function About(props) {
 			<h1>Gallery</h1>
 			<h2>Subheading Stuff</h2>
 			<span className='grid-template'>
-				
-				<img src="" alt="" srcset="" />
+				<img src={'../../assets/456373006516-R1-035-16.jpg'} alt='' />
+				<img src={'../../assets/clouds.jpg'} alt='' />
+				<img src={'../../assets/DSCF0089.jpg'} alt='' />
+				<img src={'../../assets/DSCF0095.jpg'} alt='' />
 			</span>
 		</div>
 	);
