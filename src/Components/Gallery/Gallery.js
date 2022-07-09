5ea7657c2b151174267e51d8769de69c695fc286
joesphchang <joesphschang@gmail.com>
2022-07-09 12:09:39 -0400

    Made new branch & connected api to gallery

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -2,22 +2,35 @@
 import './Gallery.css';
 
 // External Stuff
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom'
 
 function About(props) {
+	const [photos, setPhotos] = useState([]);
+
+	useEffect(() => {
+		fetch('https://photosetc-backend.herokuapp.com/api/photos')
+			.then((res) => res.json())
+			.then((res) => {
+				setPhotos(res);
+			});
+	}, []);
+
 	return (
 		<div className='body-container'>
 			<article className='page-header'>
 				<h1 className='page-heading'>Gallery</h1>
 				<Link to={'/submit-image'}><button className='page-button'>Post Image</button></Link>
 			</article>
-			<section className='gallery-container'>
-				<img src={'../../assets/456373006516-R1-035-16.jpg'} alt='' />
-				<img src={'../../assets/clouds.jpg'} alt='' />
-				<img src={'../../assets/DSCF0089.jpg'} alt='' />
-				<img src={'../../assets/DSCF0095.jpg'} alt='' />
-			</section>
+			<ul className='gallery-container'>
+				{photos.map((photo) => {
+					return ( 
+						<li>
+							<img src={photo.image} alt={photo.description} />
+						</li>
+					)
+				})}
+			</ul>
 		</div>
 	);
 }
